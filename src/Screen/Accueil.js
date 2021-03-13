ee9fa0b2f32a90553b189bf77b129fbebbdcf7c3
mouad.zizi <mouadzizi@users.noreply.github.com>
2021-03-13 14:50:40 +0100

    commit

--- src/Screen/Accueil.js
+++ src/Screen/Accueil.js
@@ -1,11 +1,44 @@
 import React from 'react'
-import { View, Text, StatusBar } from 'react-native'
+import { View, Text, StatusBar, TouchableHighlight, StyleSheet, FlatList, Alert} from 'react-native'
+
+const DATA=[ 'first item', 'second item', 'third item', 'g', 'h', 'j', 'kk']
+
+
+const Item = ({ item }) => (
+    <TouchableHighlight style={styles.touch}>
+                 <Text style={styles.text}> {item} </Text>  
+    </TouchableHighlight>
+  );
 
 export default function Acceuil() {
     return (
         <View>
         <StatusBar backgroundColor="#ffc814"/>
-            <Text>Home</Text>
+
+            <FlatList
+                data={DATA}
+                renderItem={Item}
+                keyExtractor={(item)=> item}
+                
+            />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      paddingTop: StatusBar.currentHeight,
+      marginHorizontal: 16
+    },
+    touch: {
+        textAlign : 'center',
+        height: 40,
+        backgroundColor: '#c2c2c2',
+        marginVertical: 20,
+    },
+    text : {
+        textAlign: 'center', fontSize: 18,
+    }
+    
+  });
