dfcf0745767c84b81a8379366db4b64e57d7783f
M-ELMOUSSAOUI <mosawi.man@live.fr>
2021-05-02 21:37:31 +0000

    test

--- src/Screen/Home/Home.js
+++ src/Screen/Home/Home.js
@@ -69,9 +69,9 @@ export default function Acceuil({ navigation }) {
     <ScrollView style={styles.container}> 
       <StatusBar backgroundColor="#ffc814" barStyle="dark-content" />
       <Welcome name="MOAD"/>
-      <LevelList title="Collége" DATA={COLLEGE_DATA} click={() => navigation.navigate("LevelDetails", { level: item })}/>
-      <LevelList title="Lycée" DATA={LYCEE_DATA} click={() => navigation.navigate("LevelDetails", { level: item })}/>
-      <LevelList title="Etude Supérieur" DATA={SUP_DATA} click={() => navigation.navigate("LevelDetails", { level: item })}/>
+      <LevelList title="Collége" DATA={COLLEGE_DATA} click={(item) => navigation.navigate("LevelDetails", { level: item })}/>
+      <LevelList title="Lycée" DATA={LYCEE_DATA} click={(item) => navigation.navigate("LevelDetails", { level: item })}/>
+      <LevelList title="Etude Supérieur" DATA={SUP_DATA} click={(item) => navigation.navigate("LevelDetails", { level: item })}/>
  
     </ScrollView>
   );
--- src/Screen/Home/LevelLists/LevelList.js
+++ src/Screen/Home/LevelLists/LevelList.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { FlatList, Text, View} from 'react-native'
 import styles from './LevelList.style'
 import LevelItem from '../../../Component/LevelItem'
+import stylesI from '../../../Component/LevelItem/LevelItem.style';
+
+const logo= require('../../../../assets/icon.png');
 
 export default function LevelList({DATA, title, click}) {
     return (
@@ -15,7 +18,28 @@ export default function LevelList({DATA, title, click}) {
         data={DATA}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <LevelItem background={item.background} title={item.title} onClick={click}/>
+          <TouchableOpacity style={stylesI.container} onPress={()=>click(item)}>
+          <ImageBackground
+            style={stylesI.backgroundStyle}
+            source={item.background}
+            imageStyle={{
+              resizeMode: 'cover',
+              borderRadius: 15,
+            }}
+          >
+            <View style={stylesI.containerImage}></View>
+    
+            <View style={stylesI.containerInfo}>
+              <View style={stylesI.containerLogo}>
+                  <Image source={logo} style={stylesI.brandLogo}/>
+              </View>
+    
+              <View style={stylesI.containerDetails}>
+                <Text style={stylesI.title}>{item.title}</Text>
+              </View>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
 
         )}
       />
